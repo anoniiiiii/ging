f5f6ff47c9db7d5b9e1ca289a618f27ea5395193
Huỳnh Long <longhb@Huynhs-MacBook-Pro.local>
2020-06-16 15:23:34 +0700

    Add CV page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import CV from './components/cv';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cv">CV</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/cv">
+          <CV />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
+const Home = () => {
+  return <h1>This is Home page</h1>;
+}
 export default App;
